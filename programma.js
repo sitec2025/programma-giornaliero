123931d2ff0fd4d0bde65b31e17f53d9c888a0fc
sitec2025 <assistenza@sitec-impianti.it>
2025-07-23 09:27:37 +0200

    Create programma.js

---
+++ programma.js
@@ -0,0 +1,14 @@
+const dataProgramma = [
+  {
+    cantiere: "Cantiere A",
+    luogo: "Torino",
+    lavorazioni: "Montaggio ponteggi",
+    collaboratori: "Anna, Luca"
+  },
+  {
+    cantiere: "Cantiere B",
+    luogo: "Milano",
+    lavorazioni: "Installazione impianti",
+    collaboratori: "Marco, Giulia"
+  }
+];
